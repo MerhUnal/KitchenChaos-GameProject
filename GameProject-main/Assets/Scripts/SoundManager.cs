1d23cb90c379d60d44554436af3f9d98b558eb26
MerhUnal <merihunal2003@gmail.com>
2024-07-15 13:55:27 +0300

    Options-Audio Levels

--- GameProject-main/Assets/Scripts/SoundManager.cs
+++ GameProject-main/Assets/Scripts/SoundManager.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
 
-    public static SoundManager Instance {  get; private set; }
-    
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+
+
+    public static SoundManager Instance { get; private set; }
+
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    private float volume = 1f;
 
 
     private void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
     private void Start()
     {
@@ -22,7 +29,7 @@ public class SoundManager : MonoBehaviour
         Player.Instance.OnPickedSomething += Player_OnPickedSomething;
         BaseCounter.OnAnyObjectPlacedHere += BaseCounter_OnAnyObjectPlacedHere;
         TrashCounter.OnAnyObjectTrashed += TrashCounter_OnAnyObjectTrashed;
-            
+
     }
 
     private void TrashCounter_OnAnyObjectTrashed(object sender, System.EventArgs e)
@@ -34,18 +41,18 @@ public class SoundManager : MonoBehaviour
     private void BaseCounter_OnAnyObjectPlacedHere(object sender, System.EventArgs e)
     {
         BaseCounter baseCounter = sender as BaseCounter;
-        PlaySound(audioClipRefsSO.objectDrop,baseCounter.transform.position);
+        PlaySound(audioClipRefsSO.objectDrop, baseCounter.transform.position);
     }
 
     private void Player_OnPickedSomething(object sender, System.EventArgs e)
     {
-        PlaySound(audioClipRefsSO.objectPickup,Player.Instance.transform.position);
+        PlaySound(audioClipRefsSO.objectPickup, Player.Instance.transform.position);
     }
 
     private void CuttingCounter_OnAnyCut(object sender, System.EventArgs e)
     {
-         
-       CuttingCounter cuttingCounter = sender as CuttingCounter;
+
+        CuttingCounter cuttingCounter = sender as CuttingCounter;
         PlaySound(audioClipRefsSO.chop, cuttingCounter.transform.position);
     }
 
@@ -63,17 +70,38 @@ public class SoundManager : MonoBehaviour
 
     private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
     {
-        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position ,volume);    
-        
-    }
-     
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
-    {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+
     }
 
-    public void PLayFootstepSound (Vector3 position, float volume)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
+    {
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
+    }
+
+    public void PLayFootstepSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footsteps, position, volume);
     }
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+
+
+        if (volume > 1f)
+        {
+
+            volume = 0f;
+        }
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+
+        
+    }
+    public float GetVolume()
+    {
+            return volume;
+    }
 }
+
