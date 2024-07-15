6614b4a7f8fadb2aeb1ce46bbd5af38ca0ccaff3
MerhUnal <merihunal2003@gmail.com>
2024-07-15 22:32:49 +0300

    Options-Key Rebinding,Controller Input-Menu Navigation

--- GameProject-main/Assets/Scripts/UI/GamePauseUI.cs
+++ GameProject-main/Assets/Scripts/UI/GamePauseUI.cs
@@ -27,8 +27,8 @@ public class GamePauseUI : MonoBehaviour
         });
 
         optionsButton.onClick.AddListener(() => {
-
-            OptionsUI.Instance.Show();
+            Hide();
+            OptionsUI.Instance.Show(Show);
         });
     }
 
@@ -56,6 +56,8 @@ public class GamePauseUI : MonoBehaviour
     private void Show()
     {
         gameObject.SetActive(true);
+
+        resumeButton.Select();
     }
 
     private void Hide()
