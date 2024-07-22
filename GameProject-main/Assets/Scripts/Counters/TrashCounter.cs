3f9712c51fc0e02459b5ab7dde341e1c42ada2dd
selinnoz <73435487+selinnoz@users.noreply.github.com>
2024-07-22 19:39:03 +0300

    TrashCounter update

--- GameProject-main/Assets/Scripts/Counters/TrashCounter.cs
+++ GameProject-main/Assets/Scripts/Counters/TrashCounter.cs
@@ -3,21 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrashCounter : BaseCounter
+public class TrashCounter : BaseCounter, IHasProgress
 {
     public static event EventHandler OnAnyObjectTrashed;
+    public static event EventHandler OnTrashFull;
+    public static event EventHandler OnTrashEmptied;
 
-  new  public static void ResetStaticData()
-  {
+    public event EventHandler<IHasProgress.OnprogressChangedEventArgs> OnProgressChanged;
+
+    private int trashCount;
+    private const int maxTrashCount = 4;
+
+    new public static void ResetStaticData()
+    {
         OnAnyObjectTrashed = null;
-  }
+        OnTrashFull = null;
+        OnTrashEmptied = null;
+    }
+
     public override void Interact(Player player)
     {
-       if (player.HasKitchenObject())
+        if (trashCount < maxTrashCount)
         {
-            player.GetKitchenObject().DestroySelf();
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().DestroySelf();
+                trashCount++;
+                OnAnyObjectTrashed?.Invoke(this, EventArgs.Empty);
 
-            OnAnyObjectTrashed?.Invoke(this,  EventArgs.Empty);
+                OnProgressChanged?.Invoke(this, new IHasProgress.OnprogressChangedEventArgs
+                {
+                    progressNormalized = (float)trashCount / maxTrashCount
+                });
+
+                if (trashCount >= maxTrashCount)
+                {
+                    OnTrashFull?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+        else
+        {
+            // Trash is full, cannot add more until emptied
+            Debug.Log("Trash is full. Press F to empty.");
+        }
+    }
+
+    public override void InteractAlternate(Player player)
+    {
+        if (trashCount >= maxTrashCount)
+        {
+            trashCount = 0;
+            OnTrashEmptied?.Invoke(this, EventArgs.Empty);
+            OnProgressChanged?.Invoke(this, new IHasProgress.OnprogressChangedEventArgs
+          {
+                progressNormalized = (float)trashCount / maxTrashCount
+            });
+            Debug.Log("Trash emptied");
         }
     }
 }
